7dc19113e8b34c9d3778dbb6d55322eed4791324
Akash Ratheeah <akashratheesh@gmail.com>
2021-10-13 15:58:04 -0600

    Minor Changes in Startup

--- OpenAI-TF_GPU-JupyterLab-VNC/startup.sh
+++ OpenAI-TF_GPU-JupyterLab-VNC/startup.sh
@@ -5,7 +5,7 @@
 ratpoison & 
 
 # Start Xterm
-xterm -e jupyter-lab --no-browser --port=8000 --ip=0.0.0.0 --allow_root &
+xterm -e jupyter-lab --no-browser --port=8000 --ip=0.0.0.0 --allow-root &
 
 # Start VNC Server
 x11vnc --usepw --forever
